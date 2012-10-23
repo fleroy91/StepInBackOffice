b18cbc1ae54b3aea674deb1268be9b1b2bb01e36
fred <fleroy@step-in.fr>
2012-10-23 13:52:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StepInBackOffice::Application.config.secret_token = '59c26a43fd9899e799691a16eee18144f830aa401263e891f7caeab19a240abe98c433029e8a7fe9570bb26e5f9084916251e4295e3e00699c3e55c30da07f70'
