2934222bbe4217f42362a4d2ba3804aa677141a5
fred <fleroy@step-in.fr>
2013-01-28 18:09:09 +0100

    Adding social rewards to init_mobile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -266,8 +266,8 @@ class HomeController < ApplicationController
       user = callSR("/collections/4ff6f9851b338a3e72000c64/entries", {:m_url => user_url}, user_url)
       user = user["array"]["resources"][0]
       rews = callSR("/collections/4ff6f04c1b338a3e720006cd/entries", { 'user.url' => user_url,
-                      'when!gte' => now.iso8601, 
-                      :sort => 'when', 
+                      'when!gte' => now.iso8601,
+                      :sort => 'when',
                       :order => 'desc'}, user_url)
       #logger.debug "Rews = #{rews.inspect}"
       rews = rews["array"]["resources"] if rews
@@ -287,11 +287,11 @@ class HomeController < ApplicationController
     # to cache the search, we trunc the lat long in order to grab shops in the same area
     lat = lat[0..4]
     lng = lng[0..5]
-    shops = callSR("/collections/4ff6ed1e1b338a5c1e000094/entries", 
+    shops = callSR("/collections/4ff6ed1e1b338a5c1e000094/entries",
         { 'location!near' => "((#{lat},#{lng}),5000)", 'beancode!gt' => 0}, "shops_#{lat}_#{lng}")
     #logger.debug "Shops : #{shops.inspect}"
     shops = shops["array"]["resources"]
-    
+
     shops.each { |shop|
       catalogs = []
       shop["catalogs"].each { |catalog|
@@ -302,6 +302,12 @@ class HomeController < ApplicationController
         catalogs.push(cat)
       }
       shop["catalogs"] = catalogs
+      rews = callSR("/collections/4ff6f04c1b338a3e720006cd/entries", { 'shop.url' => shop["m_url"],
+                'when!gte' => now.iso8601,
+                :sort => 'when',
+                :order => 'desc'}, "shops_#{lat}_#{lng}")
+      rews = rews["array"]["resources"] if rews
+      shop["social_rewards"] = rews
     }
     user["shops"] = shops
 
@@ -341,8 +347,8 @@ private
     @ranking_shops = []
     current_user.get_shops().each { |shop|
       @ranking_shops.push(
-        [shop.name, 
-          countObj(objs, "stepin", shop), 
+        [shop.name,
+          countObj(objs, "stepin", shop),
           countObj(objs, "catalog", shop),
           (countObj(objs, "catalog", shop) * 1.37).round,
           sumObj(objs, shop)])
@@ -355,9 +361,9 @@ private
     logger.debug "Chart = #{@chart.inspect}"
 
   end
-  def countObj(objs, ak, shop = nil) 
+  def countObj(objs, ak, shop = nil)
     sum = 0
-    objs.each { |obj| 
+    objs.each { |obj|
       if obj.action_kind == ak then
         if shop.nil? || obj.shop.attributes['url'] == shop.m_url || obj.shop.attributes['m_url'] == shop.m_url then
           sum = sum+1
@@ -367,9 +373,9 @@ private
     return sum
   end
 
-  def sumObj(objs, shop = nil) 
+  def sumObj(objs, shop = nil)
     sum = 0
-    objs.each { |obj| 
+    objs.each { |obj|
       if shop.nil? || obj.shop.attributes['url'] == shop.m_url || obj.shop.attributes['m_url'] == shop.m_url then
         sum = sum+obj.nb_points
       end
@@ -424,7 +430,7 @@ private
     logger.debug "Scans = #{scans.inspect}"
     @nb_scans = scans.size
 
-    scans.each { |scan| 
+    scans.each { |scan|
       serie = generate_serie([:all], objs, scan)
       logger.debug "Serie = #{serie.inspect}"
       data.push [scan.title, serie[0]]
